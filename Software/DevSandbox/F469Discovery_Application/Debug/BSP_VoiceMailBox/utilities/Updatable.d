5dea93fd4db993ecccd5d8c9e0457cf1218c64f1
Dominik Iten <dominik.iten@ost.ch>
2025-06-02 14:22:43 +0200

    rm debug data

--- Software/DevSandbox/F469Discovery_Application/Debug/BSP_VoiceMailBox/utilities/Updatable.d
+++
@@ -1,4 +0,0 @@
-BSP_VoiceMailBox/utilities/Updatable.o: \
- /home/dominik/Documents/studium/sem08/SA/Voicemail-Box/Software/BSP_VoiceMailBox/src/utilities/Updatable.cpp \
- /home/dominik/Documents/studium/sem08/SA/Voicemail-Box/Software/DevSandbox/F469Discovery_Application/../../BSP_VoiceMailBox/inc/utilities/Updatable.hpp
-/home/dominik/Documents/studium/sem08/SA/Voicemail-Box/Software/DevSandbox/F469Discovery_Application/../../BSP_VoiceMailBox/inc/utilities/Updatable.hpp:
